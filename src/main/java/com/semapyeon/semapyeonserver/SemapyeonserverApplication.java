93e8ba1eefe0056e8eccb41f8f530929344c8f33
sansan20535 <20san20@daum.net>
2024-07-02 15:41:04 +0900

    init project

---
+++ src/main/java/com/semapyeon/semapyeonserver/SemapyeonserverApplication.java
@@ -0,0 +1,13 @@
+package com.semapyeon.semapyeonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemapyeonserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemapyeonserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semapyeon/semapyeonserver/SemapyeonserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semapyeon.semapyeonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemapyeonserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

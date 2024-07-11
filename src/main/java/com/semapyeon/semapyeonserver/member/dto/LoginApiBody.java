005c47f092bfb219ba9cbdc310664837bd4235a8
Sumin <new.sumyang@gmail.com>
2024-07-12 01:51:25 +0900

    [refactor] : 코드 정리

--- src/main/java/com/semapyeon/semapyeonserver/member/dto/LoginApiBody.java
+++ src/main/java/com/semapyeon/semapyeonserver/member/dto/LoginApiBody.java
@@ -3,8 +3,8 @@ package com.semapyeon.semapyeonserver.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
 public class LoginApiBody {
     private String major;
     private String name;
--- src/main/java/com/semapyeon/semapyeonserver/member/service/MemberServiceImpl.java
+++ src/main/java/com/semapyeon/semapyeonserver/member/service/MemberServiceImpl.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-@Service
 @Slf4j
+@Service
 public class MemberServiceImpl implements MemberService {
 
     @Override

768f5eec440bf60806ef30c0a09dd6b910e08bcf
Sumin <new.sumyang@gmail.com>
2024-07-12 02:00:54 +0900

    [refactor] : service 수정, controller 수정

--- src/main/java/com/semapyeon/semapyeonserver/member/controller/MemberController.java
+++ src/main/java/com/semapyeon/semapyeonserver/member/controller/MemberController.java
@@ -4,11 +4,13 @@ import com.semapyeon.semapyeonserver.member.dto.LoginRequest;
 import com.semapyeon.semapyeonserver.member.dto.Member;
 import com.semapyeon.semapyeonserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
@@ -18,6 +20,7 @@ public class MemberController {
     @PostMapping("/api/v1/login")
     public ResponseEntity login(@RequestBody LoginRequest loginRequest) {
         Member member = memberService.loginMember(loginRequest);
+        log.info("member's major = {}, member's name ={}", member.getMajor(), member.getName());
         return null; //일단 빨간줄 없애려고..
     }
 }

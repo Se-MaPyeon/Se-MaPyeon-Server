fb7885c3af0529ee8e42bcb6f02ca5582d487d8a
Sumin <new.sumyang@gmail.com>
2024-07-11 18:15:41 +0900

    [modify]: 어노테이션추가

--- src/main/java/com/semapyeon/semapyeonserver/member/LoginApiMember.java
+++ src/main/java/com/semapyeon/semapyeonserver/member/LoginApiMember.java
@@ -1,5 +1,10 @@
 package com.semapyeon.semapyeonserver.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginApiMember {
     private String major;
     private String name;

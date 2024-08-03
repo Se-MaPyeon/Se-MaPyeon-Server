8ca9b09fb81afdd316b1509d10837d9635a56c47
Wonjun Lee <72507602+firepool7030@users.noreply.github.com>
2024-08-04 00:14:05 +0900

    [feat]: Cors CRUD연동 double check

---
+++ src/main/java/com/semapyeon/semapyeonserver/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.semapyeon.semapyeonserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000", "http://43.200.67.235:8080", "https://semapyeon.netlify.app")
+                        .allowedMethods("GET", "POST", PUT, "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
